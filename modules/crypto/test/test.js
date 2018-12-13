ad161c5f3174ecb2c1875f5abd15bebc89a6a982
Benjamin Mayr <mail@benjamin-mayr.com>
2018-12-13 15:20:21 +0100

    Cleanup files, replace 'var' with 'const' for imports, use 4-character-wide tab for indentation

--- modules/crypto/test/test.js
+++ modules/crypto/test/test.js
@@ -1,19 +1,19 @@
-var mod = require("../build/module");
+const mod = require("../build/module");
 
 const input = {
-    input: {},
-    context: {
-        getFullContext: () => input.context
-    }
+	input: {},
+	context: {
+		getFullContext: () => input.context
+	}
 };
 
 (async () => {
-    try {
-        let result = await mod.encrypt(input, { "algorithm": "aes-192-cfb", "key": "heltewig", "text": "philipp", "writeToContext": false, "store": "encrypted" });
-        let result2 = await mod.decrypt(result, { "algorithm": "aes-192-cfb", "key": "heltewig", "text": input.input.encrypted.result, "writeToContext": false, "store": "decrypted" });
-        let result3 = await mod.createHash(result2, { "algorithm": "md5", "key": "heltewig", "text": input.input.decrypted.result, "writeToContext": true, "store": "hash" });
-        console.log(JSON.stringify(result3, undefined, 4));
-    } catch (err) {
-        console.log(err);
-    }
+	try {
+		let result = await mod.encrypt(input, { "algorithm": "aes-192-cfb", "key": "heltewig", "text": "philipp", "writeToContext": false, "store": "encrypted" });
+		let result2 = await mod.decrypt(result, { "algorithm": "aes-192-cfb", "key": "heltewig", "text": input.input.encrypted.result, "writeToContext": false, "store": "decrypted" });
+		let result3 = await mod.createHash(result2, { "algorithm": "md5", "key": "heltewig", "text": input.input.decrypted.result, "writeToContext": true, "store": "hash" });
+		console.log(JSON.stringify(result3, undefined, 4));
+	} catch (err) {
+		console.log(err);
+	}
 })(); 
