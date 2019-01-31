5fbb9aa3ac4e74b939cd8b46c4796faa66c2d9fa
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-01-31 15:32:41 +0100

    changed string to CognigyScript

--- modules/salesforce/src/module.ts
+++ modules/salesforce/src/module.ts
@@ -47,7 +47,7 @@ module.exports.SOQLQuery = SOQLQuery;
 /**
  * Describes the function
  * @arg {SecretSelect} `secret` The configured secret to use
- * @arg {String} `option` The entity type to create
+ * @arg {CognigyScript} `option` The entity type to create
  * @arg {JSON} `info` The information as JSON
  * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
  * @arg {CognigyScript} `store` Where to store the result
@@ -94,8 +94,8 @@ module.exports.createEntity = createEntity;
 /**
  * Describes the function
  * @arg {SecretSelect} `secret` The configured secret to use
- * @arg {String} `option` The entity type to retrieve
- * @arg {String} `entity_id` of the entitity to retrieve
+ * @arg {CognigyScript} `option` The entity type to retrieve
+ * @arg {CognigyScript} `entity_id` of the entitity to retrieve
  * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
  * @arg {CognigyScript} `store` Where to store the result
  * @arg {Boolean} `stopOnError` Whether to stop on error or continue
@@ -141,8 +141,8 @@ module.exports.retrieveEntity = retrieveEntity;
 /**
  * Describes the function
  * @arg {SecretSelect} `secret` The configured secret to use
- * @arg {String} `option` The entity type to delete
- * @arg {String} `entity_id` of the entitity to delete
+ * @arg {CognigyScript} `option` The entity type to delete
+ * @arg {CognigyScript} `entity_id` of the entitity to delete
  * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
  * @arg {CognigyScript} `store` Where to store the result
  * @arg {Boolean} `stopOnError` Whether to stop on error or continue
