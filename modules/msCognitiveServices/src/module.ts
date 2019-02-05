871b3b927908c25b2441ef90bc4b081f242716d3
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-02-05 14:01:27 +0100

    added func extractKeyphrases for en,de,es

--- modules/msCognitiveServices/src/module.ts
+++ modules/msCognitiveServices/src/module.ts
@@ -1,7 +1,7 @@
 let https = require ('https');
 
 /**
- * Describes the function
+ * finds spelling mistakes and predicts the correct word
  * @arg {SecretSelect} `secret` The configured secret to use
  * @arg {CognigyScript} `text` The text to check
  * @arg {Select[ar,zh-CN,zh-HK,zh-TW,da,nl-BE,nl-NL,en-AU,en-CA,en-IN,en-ID,en-MY,en-NZ,en-PH,en-ZA,en-GB,en-US,fi,fr-BE,fr-CA,fr-FR,fr-CH,de-AT,de-DE,de-CH,it,ja,ko,no,pl,pt-BR,pt-PT,ru,es-AR,es-CL,es-MX,es-ES,es-US,sv,tr]} `language` The texts language
@@ -73,14 +73,14 @@ module.exports.spellCheck = spellCheck;
 
 
 /**
- * Describes the function
+ * recognize the language of the given sentence
  * @arg {SecretSelect} `secret` The configured secret to use
  * @arg {CognigyScript} `text` The text to check
  * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
  * @arg {CognigyScript} `store` Where to store the result
  * @arg {Boolean} `stopOnError` Whether to stop on error or continue
  */
-async function textAnalytics(input: IFlowInput, args: { secret: CognigySecret, text: string, writeToContext: boolean, store: string, stopOnError: boolean }): Promise<IFlowInput | {}>  {
+async function recognizeLanguage(input: IFlowInput, args: { secret: CognigySecret, text: string, writeToContext: boolean, store: string, stopOnError: boolean }): Promise<IFlowInput | {}>  {
     // Check if secret exists and contains correct parameters
     if (!args.secret || !args.secret.key) return Promise.reject("Secret not defined or invalid.");
     if (!args.text) return Promise.reject("No text defined.");
@@ -147,4 +147,84 @@ async function textAnalytics(input: IFlowInput, args: { secret: CognigySecret, t
 }
 
 // You have to export the function, otherwise it is not available
-module.exports.textAnalytics = textAnalytics;
+module.exports.recognizeLanguage = recognizeLanguage;
+
+
+/**
+ * extracts keyphrases from a given sentence
+ * @arg {SecretSelect} `secret` The configured secret to use
+ * @arg {Select[en,es,de]} `language` The texts language
+ * @arg {CognigyScript} `text` The text to check
+ * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
+ * @arg {CognigyScript} `store` Where to store the result
+ * @arg {Boolean} `stopOnError` Whether to stop on error or continue
+ */
+async function extractKeyphrases(input: IFlowInput, args: { secret: CognigySecret, language: string, text: string, writeToContext: boolean, store: string, stopOnError: boolean }): Promise<IFlowInput | {}>  {
+    // Check if secret exists and contains correct parameters
+    if (!args.secret || !args.secret.key) return Promise.reject("Secret not defined or invalid.");
+    if (!args.text) return Promise.reject("No text defined.");
+
+    return new Promise((resolve, reject) => {
+        let result = {};
+
+        let accessKey = args.secret.key;
+
+        // You must use the same region in your REST API call as you used to obtain your access keys.
+        // For example, if you obtained your access keys from the westus region, replace
+        // "westcentralus" in the URI below with "westus".
+
+        // NOTE: Free trial access keys are generated in the westcentralus region, so if you are using
+        // a free trial access key, you should not need to change this region.
+        let uri = 'westus.api.cognitive.microsoft.com';
+        let path = '/text/analytics/v2.0/keyPhrases';
+
+        let response_handler = function (response) {
+            let body = '';
+            response.on ('data', function (d) {
+                body += d;
+            });
+            response.on ('end', function () {
+                result = JSON.parse(body);
+                if (args.writeToContext) input.context.getFullContext()[args.store] = result;
+                else input.input[args.store] = result;
+                resolve(input);
+            });
+            response.on ('error', function (err) {
+                if (args.stopOnError) { reject(err.message); return; }
+                result = { "error": err.message };
+                if (args.writeToContext) input.context.getFullContext()[args.store] = result;
+                else input.input[args.store] = result;
+                resolve(input);
+            });
+        };
+
+        let get_key_phrases = function (documents) {
+            let body = JSON.stringify (documents);
+
+            let request_params = {
+                method : 'POST',
+                hostname : uri,
+                path : path,
+                headers : {
+                    'Ocp-Apim-Subscription-Key' : accessKey,
+                }
+            };
+
+            let req = https.request (request_params, response_handler);
+            req.write (body);
+            req.end ();
+        };
+
+        let documents = { 'documents': [
+                { 'id': '1', 'language': args.language, 'text': args.text }
+            ]};
+
+        get_key_phrases (documents);
+
+    });
+}
+
+// You have to export the function, otherwise it is not available
+module.exports.extractKeyphrases = extractKeyphrases;
+
+
