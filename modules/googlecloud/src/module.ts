9699b3bb7bb9239b6983c7c22490daa7689b2f73
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-02-14 16:15:55 +0100

    added languageDetection function

--- modules/googlecloud/src/module.ts
+++ modules/googlecloud/src/module.ts
@@ -37,3 +37,41 @@ async function translation(input: IFlowInput, args: { secret: CognigySecret, lan
 // You have to export the function, otherwise it is not available
 module.exports.translation = translation;
 
+
+/**
+ * detects the language of a given sentence
+ * @arg {SecretSelect} `secret` The configured secret to use
+ * @arg {CognigyScript} `text` to detect
+ * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
+ * @arg {CognigyScript} `store` Where to store the result
+ * @arg {Boolean} `stopOnError` Whether to stop on error or continue
+ */
+async function languageDetection(input: IFlowInput, args: { secret: CognigySecret, text: string, writeToContext: boolean, store: string, stopOnError: boolean }): Promise<IFlowInput | {}> {
+  // Check if secret exists and contains correct parameters
+  if (!args.secret || !args.secret.key) return Promise.reject("Secret not defined or invalid.");
+  if (!args.text) return Promise.reject("No text defined.");
+
+  let googleTranslate = require('google-translate')(args.secret.key);
+
+  return new Promise((resolve, reject) => {
+    let result = {};
+
+    googleTranslate.detectLanguage(args.text, function(err, detection) {
+      if (err) {
+        if (args.stopOnError) { reject(err.message); return; }
+        result = { "error": err.message };
+        input.input[args.store] = result;
+        resolve(input);
+      }
+      result = detection.language;
+      if (args.writeToContext) input.context.getFullContext()[args.store] = result;
+      else input.input[args.store] = result;
+      resolve(input);
+    });
+  });
+}
+
+// You have to export the function, otherwise it is not available
+module.exports.languageDetection = languageDetection;
+
+
