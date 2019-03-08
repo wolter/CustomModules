01d4599f5b9ea6c12f1993872cd03133d6e1ec86
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-03-08 14:03:59 +0100

    deleted crashing nodes and deleted unused comments

--- modules/jira/src/module.ts
+++ modules/jira/src/module.ts
@@ -29,22 +29,7 @@ async function extractTicket(input: IFlowInput, args: {  secret: CognigySecret, 
   });
 }
 
-// You have to export the function, otherwise it is not available
 module.exports.extractTicket = extractTicket;
-
-
-
-
-
-
-
-
-/******************************************** */
-//STATUS
-/******************************************** */
-
-
-
 
 
 /**
@@ -98,13 +83,7 @@ async function getTicketStatus(input: IFlowInput, args: { secret: CognigySecret,
   });
 }
 
-// You have to export the function, otherwise it is not available
 module.exports.getTicketStatus = getTicketStatus;
-
-
-/******************************************** */
-//ASSIGNEE//
-/******************************************** */
 
 
 /**
@@ -158,16 +137,7 @@ async function getTicketAssignee(input: IFlowInput, args: { secret: CognigySecre
   });
 }
 
-// You have to export the function, otherwise it is not available
 module.exports.getTicketAssignee = getTicketAssignee;
-
-
-
-
-
-/******************************************** */
-//PRIORITY//
-/******************************************** */
 
 
 /**
@@ -221,14 +191,7 @@ async function getTicketPriority(input: IFlowInput, args: { secret: CognigySecre
   });
 }
 
-// You have to export the function, otherwise it is not available
 module.exports.getTicketPriority = getTicketPriority;
-
-
-
-/******************************************** */
-//RESOLUTION//
-/******************************************** */
 
 
 /**
@@ -282,17 +245,7 @@ async function getTicketResolution(input: IFlowInput, args: { secret: CognigySec
   });
 }
 
-// You have to export the function, otherwise it is not available
 module.exports.getTicketResolution = getTicketResolution;
-
-
-
-
-
-
-/******************************************** */
-//REPORTER
-/******************************************** */
 
 
 /**
@@ -346,82 +299,7 @@ async function getTicketReporter(input: IFlowInput, args: { secret: CognigySecre
   });
 }
 
-// You have to export the function, otherwise it is not available
 module.exports.getTicketReporter = getTicketReporter;
-
-
-
-
-
-
-
-
-/******************************************** */
-//COMMENTS
-/******************************************** */
-
-
-/**
- * Returns comments on this ticket, if it has any. 
- * @arg {SecretSelect} `secret` The configured secret to use
- * @arg {CognigyScript} `ticket` The ticket number e.g. ABC-1234
- * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
- * @arg {CognigyScript} `store` Where to store the result
- * @arg {Boolean} `stopOnError` Whether to stop on error or continue
- */
-
-async function getTicketComments(input: IFlowInput, args: { secret: CognigySecret, ticket: string, writeToContext: boolean, store: string, stopOnError: boolean }): Promise<IFlowInput | {}> {
-
-  // Check if secret exists and contains correct parameters
-  if (!args.secret || !args.secret.username || !args.secret.password  || !args.secret.domain) return Promise.reject("Secret not defined or invalid.");
-  if (!args.ticket) return Promise.reject("No ticket defined. Please define a ticket like AB-1234");
-  
-  return new Promise((resolve, reject) => {
-    let result = {};
-
-    const jira = new JiraClient( {
-      host: args.secret.domain,
-      basic_auth: {
-          username: args.secret.username,
-          password: args.secret.password
-        }
-    });
-
-    jira.issue.getIssue({
-      issueKey: args.ticket
-    }, function(error, issue) {
-        if(error) {
-          if (args.stopOnError) { reject(error.message); return; }
-          result = { "error": error.message };
-          if (args.writeToContext) input.context.getFullContext()[args.store] = result;
-          else input.input[args.store] = result;
-          resolve(input);
-        } else {
-
-          //
-
-          let result = { 
-            ticket: issue.key,
-            status: issue.fields.comments
-          }
-          if (args.writeToContext) input.context.getFullContext()[args.store] = result;
-          else input.input[args.store] = result;
-          resolve(input);
-        }
-    });
-  });
-}
-
-// You have to export the function, otherwise it is not available
-module.exports.getTicketComments = getTicketComments;
-
-
-
-
-
-/******************************************** */
-//WATCHERS
-/******************************************** */
 
 
 /**
@@ -475,85 +353,7 @@ async function getTicketWatchers(input: IFlowInput, args: { secret: CognigySecre
   });
 }
 
-// You have to export the function, otherwise it is not available
 module.exports.getTicketWatchers = getTicketWatchers;
-
-
-
-/******************************************** */
-//SUMMARY
-/******************************************** */
-
-
-/**
- * Returns basic summary of the ticket, including: type, project, status, assignedTo, reportedBy, resolution and comments. 
- * @arg {SecretSelect} `secret` The configured secret to use
- * @arg {CognigyScript} `ticket` The ticket number e.g. ABC-1234
- * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
- * @arg {CognigyScript} `store` Where to store the result
- * @arg {Boolean} `stopOnError` Whether to stop on error or continue
- */
-
-async function getTicketSummary(input: IFlowInput, args: { secret: CognigySecret, ticket: string, writeToContext: boolean, store: string, stopOnError: boolean }): Promise<IFlowInput | {}> {
-
-  // Check if secret exists and contains correct parameters
-  if (!args.secret || !args.secret.username || !args.secret.password  || !args.secret.domain) return Promise.reject("Secret not defined or invalid.");
-  if (!args.ticket) return Promise.reject("No ticket defined. Please define a ticket like AB-1234");
-  
-  return new Promise((resolve, reject) => {
-    let result = {};
-
-    const jira = new JiraClient( {
-      host: args.secret.domain,
-      basic_auth: {
-          username: args.secret.username,
-          password: args.secret.password
-        }
-    });
-
-    jira.issue.getIssue({
-      issueKey: args.ticket
-    }, function(error, issue) {
-        if(error) {
-          if (args.stopOnError) { reject(error.message); return; }
-          result = { "error": error.message };
-          if (args.writeToContext) input.context.getFullContext()[args.store] = result;
-          else input.input[args.store] = result;
-          resolve(input);
-        } else {
-
-          //
-
-          let result = { 
-            ticket: issue.key,
-            type: issue.fields.issuetype.name,
-            project: issue.fields.project.name,
-            status: issue.fields.status.name,
-            assignedTo: issue.fields.assignee.emailAddress,
-            reportedBy: issue.fields.reporter.emailAddress,
-            resolution: issue.fields.resolution.name,
-            comments: issue.fields.comment.comments
-          }
-          if (args.writeToContext) input.context.getFullContext()[args.store] = result;
-          else input.input[args.store] = result;
-          resolve(input);
-        }
-    });
-  });
-}
-
-// You have to export the function, otherwise it is not available
-module.exports.getTicketSummary = getTicketSummary;
-
-
-
-
-/******************************************** */
-//FULL TICKET
-/******************************************** */
-
-
-
 
 
 /**
@@ -600,7 +400,6 @@ async function getAllTicketInfo(input: IFlowInput, args: { secret: CognigySecret
   });
 }
 
-// You have to export the function, otherwise it is not available
 module.exports.getAllTicketInfo = getAllTicketInfo;
 
 
