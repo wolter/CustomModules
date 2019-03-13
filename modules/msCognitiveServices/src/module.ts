05576e4afce59f95a745cf0be1f4acab8c1a0ff8
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-03-13 12:28:25 +0100

    deleted writeToContext bool

--- modules/msCognitiveServices/src/module.ts
+++ modules/msCognitiveServices/src/module.ts
@@ -7,11 +7,10 @@ const uuidv4 = require('uuid/v4');
  * @arg {SecretSelect} `secret` The configured secret to use
  * @arg {CognigyScript} `text` The text to check
  * @arg {Select[ar,zh-CN,zh-HK,zh-TW,da,nl-BE,nl-NL,en-AU,en-CA,en-IN,en-ID,en-MY,en-NZ,en-PH,en-ZA,en-GB,en-US,fi,fr-BE,fr-CA,fr-FR,fr-CH,de-AT,de-DE,de-CH,it,ja,ko,no,pl,pt-BR,pt-PT,ru,es-AR,es-CL,es-MX,es-ES,es-US,sv,tr]} `language` The texts language
- * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
  * @arg {CognigyScript} `store` Where to store the result
  * @arg {Boolean} `stopOnError` Whether to stop on error or continue
  */
-async function spellCheck(input: IFlowInput, args: { secret: CognigySecret, text: string, language: string, writeToContext: boolean, store: string, stopOnError: boolean }): Promise<IFlowInput | {}>  {
+async function spellCheck(input: IFlowInput, args: { secret: CognigySecret, text: string, language: string, store: string, stopOnError: boolean }): Promise<IFlowInput | {}>  {
     // Check if secret exists and contains correct parameters
     if (!args.secret || !args.secret.key) return Promise.reject("Secret not defined or invalid.");
     if (!args.text) return Promise.reject("No text defined.");
@@ -42,15 +41,13 @@ async function spellCheck(input: IFlowInput, args: { secret: CognigySecret, text
             response.on('end', () => {
                 result = JSON.parse(body);
 
-                if (args.writeToContext) input.context.getFullContext()[args.store] = result;
-                else input.input[args.store] = result;
+                input.context.getFullContext()[args.store] = result;
                 resolve(input);
             });
             response.on('error', (err) => {
                 if (args.stopOnError) { reject(err.message); return; }
                 result = { "error": err.message };
-                if (args.writeToContext) input.context.getFullContext()[args.store] = result;
-                else input.input[args.store] = result;
+                input.context.getFullContext()[args.store] = result;
                 resolve(input);
             });
         };
@@ -68,11 +65,10 @@ module.exports.spellCheck = spellCheck;
  * Recognize the language of the given sentence.
  * @arg {SecretSelect} `secret` The configured secret to use
  * @arg {CognigyScript} `text` The text to check
- * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
  * @arg {CognigyScript} `store` Where to store the result
  * @arg {Boolean} `stopOnError` Whether to stop on error or continue
  */
-async function recognizeLanguage(input: IFlowInput, args: { secret: CognigySecret, text: string, writeToContext: boolean, store: string, stopOnError: boolean }): Promise<IFlowInput | {}>  {
+async function recognizeLanguage(input: IFlowInput, args: { secret: CognigySecret, text: string, store: string, stopOnError: boolean }): Promise<IFlowInput | {}>  {
     // Check if secret exists and contains correct parameters
     if (!args.secret || !args.secret.key || !args.secret.region) return Promise.reject("Secret not defined or invalid.");
     if (!args.text) return Promise.reject("No text defined.");
@@ -91,15 +87,13 @@ async function recognizeLanguage(input: IFlowInput, args: { secret: CognigySecre
             });
             response.on('end', () => {
                 result = JSON.parse(body);
-                if (args.writeToContext) input.context.getFullContext()[args.store] = result;
-                else input.input[args.store] = result;
+                input.context.getFullContext()[args.store] = result;
                 resolve(input);
             });
             response.on('error', (err) => {
                 if (args.stopOnError) { reject(err.message); return; }
                 result = { "error": err.message };
-                if (args.writeToContext) input.context.getFullContext()[args.store] = result;
-                else input.input[args.store] = result;
+                input.context.getFullContext()[args.store] = result;
                 resolve(input);
             });
         };
@@ -138,11 +132,10 @@ module.exports.recognizeLanguage = recognizeLanguage;
  * @arg {SecretSelect} `secret` The configured secret to use
  * @arg {Select[en,es,de]} `language` The texts language
  * @arg {CognigyScript} `text` The text to check
- * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
  * @arg {CognigyScript} `store` Where to store the result
  * @arg {Boolean} `stopOnError` Whether to stop on error or continue
  */
-async function extractKeyphrases(input: IFlowInput, args: { secret: CognigySecret, language: string, text: string, writeToContext: boolean, store: string, stopOnError: boolean }): Promise<IFlowInput | {}>  {
+async function extractKeyphrases(input: IFlowInput, args: { secret: CognigySecret, language: string, text: string, store: string, stopOnError: boolean }): Promise<IFlowInput | {}>  {
     // Check if secret exists and contains correct parameters
     if (!args.secret || !args.secret.key || !args.secret.region) return Promise.reject("Secret not defined or invalid.");
     if (!args.text) return Promise.reject("No text defined.");
@@ -161,15 +154,13 @@ async function extractKeyphrases(input: IFlowInput, args: { secret: CognigySecre
             });
             response.on('end', () => {
                 result = JSON.parse(body);
-                if (args.writeToContext) input.context.getFullContext()[args.store] = result;
-                else input.input[args.store] = result;
+                input.context.getFullContext()[args.store] = result;
                 resolve(input);
             });
             response.on('error', (err) => {
                 if (args.stopOnError) { reject(err.message); return; }
                 result = { "error": err.message };
-                if (args.writeToContext) input.context.getFullContext()[args.store] = result;
-                else input.input[args.store] = result;
+                input.context.getFullContext()[args.store] = result;
                 resolve(input);
             });
         };
@@ -208,11 +199,10 @@ module.exports.extractKeyphrases = extractKeyphrases;
  * @arg {SecretSelect} `secret` The configured secret to use
  * @arg {Select[en,es,de]} `language` The texts language
  * @arg {CognigyScript} `text` The text to check
- * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
  * @arg {CognigyScript} `store` Where to store the result
  * @arg {Boolean} `stopOnError` Whether to stop on error or continue
  */
-async function namedEntityRecognition(input: IFlowInput, args: { secret: CognigySecret, language: string, text: string, writeToContext: boolean, store: string, stopOnError: boolean }): Promise<IFlowInput | {}>  {
+async function namedEntityRecognition(input: IFlowInput, args: { secret: CognigySecret, language: string, text: string, store: string, stopOnError: boolean }): Promise<IFlowInput | {}>  {
     // Check if secret exists and contains correct parameters
     if (!args.secret || !args.secret.key || !args.secret.region) return Promise.reject("Secret not defined or invalid.");
     if (!args.text) return Promise.reject("No text defined.");
@@ -231,15 +221,13 @@ async function namedEntityRecognition(input: IFlowInput, args: { secret: Cognigy
             });
             response.on('end', () => {
                 result = JSON.parse(body);
-                if (args.writeToContext) input.context.getFullContext()[args.store] = result;
-                else input.input[args.store] = result;
+                input.context.getFullContext()[args.store] = result;
                 resolve(input);
             });
             response.on('error', (err) => {
                 if (args.stopOnError) { reject(err.message); return; }
                 result = { "error": err.message };
-                if (args.writeToContext) input.context.getFullContext()[args.store] = result;
-                else input.input[args.store] = result;
+                input.context.getFullContext()[args.store] = result;
                 resolve(input);
             });
         };
@@ -406,11 +394,10 @@ module.exports.bingImageSearch = bingImageSearch;
  * @arg {SecretSelect} `secret` The configured secret to use
  * @arg {Select[af,ar,bn,bs,bg,yue,ca,zh-Hans,zh-Hant,hr,cs,da,nl,en,et,fj,fil,fi,fr,de,el,ht,he,hi,mww,hu,is,id,it,ja,sw,tlh,tlh-Qaak,ko,lv,lt,mg,ms,mt,nb,fa,pl,pt,otq,ro,ru,sm,sr-Cyrl,sr-Latn,sk,sl,es,sv,ty,ta,te,th,to,tr,uk,ur,vi,cy,yau]} `language` to which language it should translate
  * @arg {CognigyScript} `text` The text to check
- * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
  * @arg {CognigyScript} `store` Where to store the result
  * @arg {Boolean} `stopOnError` Whether to stop on error or continue
  */
-async function textTranslator(input: IFlowInput, args: { secret: CognigySecret, language: string, text: string, writeToContext: boolean, store: string, stopOnError: boolean }): Promise<IFlowInput | {}>  {
+async function textTranslator(input: IFlowInput, args: { secret: CognigySecret, language: string, text: string, store: string, stopOnError: boolean }): Promise<IFlowInput | {}>  {
     // Check if secret exists and contains correct parameters
     if (!args.secret || !args.secret.key) return Promise.reject("Secret not defined or invalid.");
     if (!args.text) return Promise.reject("No text defined.");
@@ -440,8 +427,7 @@ async function textTranslator(input: IFlowInput, args: { secret: CognigySecret, 
 
         request(options, (err, res, body) => {
             result = body;
-            if (args.writeToContext) input.context.getFullContext()[args.store] = result;
-            else input.input[args.store] = result;
+            input.context.getFullContext()[args.store] = result;
             resolve(input);
         });
     });
