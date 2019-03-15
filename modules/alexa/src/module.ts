b41a0bf90ac89ab1e52198231efb3c6c2abc8948
wolter <sw@saschawolter.de>
2019-03-15 09:36:09 +0100

    linting added and done

--- modules/alexa/src/module.ts
+++ modules/alexa/src/module.ts
@@ -1,6 +1,6 @@
 import * as request_promise from 'request-promise';
 
-function handleError(input: IFlowInput, error:Error, stopOnError:boolean, writeToContext:boolean, store:string): IFlowInput | {}  {
+function handleError(input: IFlowInput, error: Error, stopOnError: boolean, writeToContext: boolean, store: string): IFlowInput | {} {
 
     input.actions.log("error", error.message);
 
@@ -18,22 +18,22 @@ function handleError(input: IFlowInput, error:Error, stopOnError:boolean, writeT
 
 }
 
-function isAlexa(input: IFlowInput): Boolean  {
+function isAlexa(input: IFlowInput): Boolean {
     // Check current channel...
     return (input.input.channel === "alexa");
 }
 
 /**
  * Create an "ask for permission consent" card using a list of permissions such as alexa::alerts:reminders:skill:readwrite, read::alexa:device:all:address etc.
- * 
+ *
  * @arg {CognigyScriptArray} `permissions` Permissions to confirm by the user. See documentation for possible values.
  */
-async function respondWithPermissionCard(input: IFlowInput, args: {permissions: Array<string>}): Promise<IFlowInput | {}> {
-    let jsonPayload:any = {
+async function respondWithPermissionCard(input: IFlowInput, args: { permissions: Array<string> }): Promise<IFlowInput | {}> {
+    let jsonPayload: any = {
         _cognigy: {
             _alexa: {
                 response: {
-                    card:  {
+                    card: {
                         type: "AskForPermissionsConsent",
                         permissions: args.permissions,
                     }
@@ -41,14 +41,14 @@ async function respondWithPermissionCard(input: IFlowInput, args: {permissions: 
             }
         }
     };
-    input.actions.output(null,jsonPayload);
+    input.actions.output(null, jsonPayload);
     return input;
 }
 module.exports.respondWithPermissionCard = respondWithPermissionCard;
 
 /**
  * Call Alexa API.
- * 
+ *
  * @arg {String} `path` Alexa API path to call (something like "/v1/devices/${deviceId}/settings/address")
  * @arg {JSON} `payload` The payload to send to the Alexa API
  * @arg {Select[GET,POST,PUT,DELETE]} `method` The REST method to be used
@@ -56,12 +56,12 @@ module.exports.respondWithPermissionCard = respondWithPermissionCard;
  * @arg {CognigyScript} `store` Where to store the result
  * @arg {Boolean} `stopOnError` Whether to stop on error or continue
  */
-async function callAlexaAPI(input: IFlowInput, args: { path:string, payload:any, method:string, writeToContext: boolean, store: string, stopOnError: boolean }): Promise<IFlowInput | {}> {
+async function callAlexaAPI(input: IFlowInput, args: { path: string, payload: any, method: string, writeToContext: boolean, store: string, stopOnError: boolean }): Promise<IFlowInput | {}> {
 
     // Check current channel...
     if (!isAlexa(input)) {
-        const error:Error = new Error("Wrong channel. Channel must be Amazon Alexa.");
-        return handleError(input, error, false, args.writeToContext, args.store);        
+        const error: Error = new Error("Wrong channel. Channel must be Amazon Alexa.");
+        return handleError(input, error, false, args.writeToContext, args.store);
     }
 
     try {
@@ -77,10 +77,10 @@ async function callAlexaAPI(input: IFlowInput, args: { path:string, payload:any,
             headers: { 'Authorization': `Bearer ${token}` }
         };
 
-        let result:Request;
+        let result: Request;
         // Handle request
         switch (args.method) {
-            case "GET":     
+            case "GET":
                 result = await request_promise.get(options);
                 break;
             case "POST":
@@ -88,20 +88,20 @@ async function callAlexaAPI(input: IFlowInput, args: { path:string, payload:any,
                 break;
             case "PUT":
                 result = await request_promise.put(options);
-                break;                
+                break;
             case "DELETE":
                 result = await request_promise.delete(options);
                 break;
             default:
-                result = await request_promise.get(options);              
+                result = await request_promise.get(options);
         }
-        
+
         if (args.writeToContext) input.context.getFullContext()[args.store] = result;
         else input.input[args.store] = result;
         return input;
 
     } catch (error) {
-        return handleError(input, error.error, args.stopOnError, args.writeToContext, args.store); 
+        return handleError(input, error.error, args.stopOnError, args.writeToContext, args.store);
     }
 }
 
@@ -109,7 +109,7 @@ module.exports.callAlexaAPI = callAlexaAPI;
 
 /**
  * Get current's device address.
- * 
+ *
  * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
  * @arg {CognigyScript} `store` Where to store the result
  * @arg {Boolean} `stopOnError` Whether to stop on error or continue
@@ -118,8 +118,8 @@ async function getDeviceAddress(input: IFlowInput, args: { writeToContext: boole
 
     // Check current channel...
     if (!isAlexa(input)) {
-        const error:Error = new Error("Wrong channel. Channel must be Amazon Alexa.");
-        return handleError(input, error, false, args.writeToContext, args.store);        
+        const error: Error = new Error("Wrong channel. Channel must be Amazon Alexa.");
+        return handleError(input, error, false, args.writeToContext, args.store);
     }
 
     try {
@@ -142,7 +142,7 @@ async function getDeviceAddress(input: IFlowInput, args: { writeToContext: boole
         return input;
 
     } catch (error) {
-        return handleError(input, error.error, args.stopOnError, args.writeToContext, args.store); 
+        return handleError(input, error.error, args.stopOnError, args.writeToContext, args.store);
     }
 }
 
@@ -150,7 +150,7 @@ module.exports.getDeviceAddress = getDeviceAddress;
 
 /**
  * Set a relative reminder and returns a confirmation.
- * 
+ *
  * @arg {String} `text` Reminder`s message text
  * @arg {Number} `offsetInSeconds` Time until reminding
  * @arg {Boolean} `pushNotification` Pushnotification enabled
@@ -158,23 +158,23 @@ module.exports.getDeviceAddress = getDeviceAddress;
  * @arg {CognigyScript} `store` Where to store the result
  * @arg {Boolean} `stopOnError` Whether to stop on error or continue
  */
-async function setRelativeReminder(input: IFlowInput, args: { writeToContext: boolean, store: string, text: string, offsetInSeconds:number, pushNotification:boolean, stopOnError: boolean }): Promise<IFlowInput | {}> {
+async function setRelativeReminder(input: IFlowInput, args: { writeToContext: boolean, store: string, text: string, offsetInSeconds: number, pushNotification: boolean, stopOnError: boolean }): Promise<IFlowInput | {}> {
 
     // Check current channel...
     if (!isAlexa(input)) {
-        const error:Error = new Error("Wrong channel. Channel must be Amazon Alexa.");
-        return handleError(input, error, false, args.writeToContext, args.store);        
+        const error: Error = new Error("Wrong channel. Channel must be Amazon Alexa.");
+        return handleError(input, error, false, args.writeToContext, args.store);
     }
 
     try {
 
         const token: string = input.data.context.System.apiAccessToken;
         const apiEndpoint: string = input.data.context.System.apiEndpoint;
-        
-        const locale:string = input.data.request.locale;
-        const requestTime:string = input.data.request.timestamp;    
-        
-       const body:any = {
+
+        const locale: string = input.data.request.locale;
+        const requestTime: string = input.data.request.timestamp;
+
+        const body: any = {
             requestTime: requestTime,
             trigger: {
                 type: "SCHEDULED_RELATIVE",
@@ -193,7 +193,7 @@ async function setRelativeReminder(input: IFlowInput, args: { writeToContext: bo
             pushNotification: {
                 status: args.pushNotification ? "ENABLED" : "DISABLED"
             }
-        }
+        };
 
         // Update these options with the details of the web service you would like to call
         const options: request_promise.OptionsWithUri = {
@@ -210,7 +210,7 @@ async function setRelativeReminder(input: IFlowInput, args: { writeToContext: bo
         return input;
 
     } catch (error) {
-        return handleError(input, error.error, args.stopOnError, args.writeToContext, args.store); 
+        return handleError(input, error.error, args.stopOnError, args.writeToContext, args.store);
     }
 }
 
@@ -218,7 +218,7 @@ module.exports.setRelativeReminder = setRelativeReminder;
 
 /**
  * Set an absolute reminder and returns a confirmation.
- * 
+ *
  * @arg {String} `text` Reminder`s message text
  * @arg {String} `scheduledTime` Scheduled time in valid ISO 8601 format without timezone
  * @arg {String} `timeZoneId` Timezone according to https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
@@ -235,38 +235,39 @@ module.exports.setRelativeReminder = setRelativeReminder;
  * @arg {CognigyScript} `store` Where to store the result
  * @arg {Boolean} `stopOnError` Whether to stop on error or continue
  */
-async function setAbsoluteReminder(input: IFlowInput, args: { 
-    writeToContext: boolean, store: string, 
-    text: string, 
-    scheduledTime:string, timeZoneId:string, frequency:String, 
-    monday:boolean, tuesday:boolean, wednesday:boolean, thursday: boolean, friday:boolean, saturday:boolean, sunday:boolean,    
-    pushNotification:boolean, stopOnError: boolean }): Promise<IFlowInput | {}> {
+async function setAbsoluteReminder(input: IFlowInput, args: {
+    writeToContext: boolean, store: string,
+    text: string,
+    scheduledTime: string, timeZoneId: string, frequency: String,
+    monday: boolean, tuesday: boolean, wednesday: boolean, thursday: boolean, friday: boolean, saturday: boolean, sunday: boolean,
+    pushNotification: boolean, stopOnError: boolean
+}): Promise<IFlowInput | {}> {
 
     // Check current channel...
     if (!isAlexa(input)) {
-        const error:Error = new Error("Wrong channel. Channel must be Amazon Alexa.");
-        return handleError(input, error, false, args.writeToContext, args.store);        
+        const error: Error = new Error("Wrong channel. Channel must be Amazon Alexa.");
+        return handleError(input, error, false, args.writeToContext, args.store);
     }
 
     try {
 
         const token: string = input.data.context.System.apiAccessToken;
         const apiEndpoint: string = input.data.context.System.apiEndpoint;
-        const days:Array<string> = new Array<string>();
-        const locale:string = input.data.request.locale;
-        const requestTime:string = input.data.request.timestamp;
+        const days: Array<string> = new Array<string>();
+        const locale: string = input.data.request.locale;
+        const requestTime: string = input.data.request.timestamp;
 
-        let recurrence:any;
+        let recurrence: any;
 
         switch (args.frequency) {
             case "DAILY":
-                recurrence = {                     
-                    freq : args.frequency,         
+                recurrence = {
+                    freq: args.frequency,
                 };
                 break;
             case "WEEKLY":
-                recurrence = {                     
-                    freq : args.frequency,         
+                recurrence = {
+                    freq: args.frequency,
                     byDay: []
                 };
                 if (args.monday) recurrence.byDay.push("MO");
@@ -278,29 +279,28 @@ async function setAbsoluteReminder(input: IFlowInput, args: {
                 if (args.sunday) recurrence.byDay.push("SU");
                 break;
             default:
-                // nothing
-        } 
+            // nothing
+        }
 
-        const body:any =  
-        {
-            requestTime : requestTime,
+        const body: any = {
+            requestTime: requestTime,
             trigger: {
-                 type : "SCHEDULED_ABSOLUTE",
-                 scheduledTime : args.scheduledTime,
-                 timeZoneId : args.timeZoneId,
-                 recurrence : recurrence,
+                type: "SCHEDULED_ABSOLUTE",
+                scheduledTime: args.scheduledTime,
+                timeZoneId: args.timeZoneId,
+                recurrence: recurrence,
             },
             alertInfo: {
-                 spokenInfo: {
-                     content: [{
-                         locale: locale, 
-                         text: args.text
-                     }]
-                 }
-             },
-             pushNotification : {                            
-                  "status" : args.pushNotification?"ENABLED":"DISABLED"
-             }
+                spokenInfo: {
+                    content: [{
+                        locale: locale,
+                        text: args.text
+                    }]
+                }
+            },
+            pushNotification: {
+                "status": args.pushNotification ? "ENABLED" : "DISABLED"
+            }
         };
 
         // Update these options with the details of the web service you would like to call
@@ -318,7 +318,7 @@ async function setAbsoluteReminder(input: IFlowInput, args: {
         return input;
 
     } catch (error) {
-        return handleError(input, error.error, args.stopOnError, args.writeToContext, args.store); 
+        return handleError(input, error.error, args.stopOnError, args.writeToContext, args.store);
     }
 }
 
