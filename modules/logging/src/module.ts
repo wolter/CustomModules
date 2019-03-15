d3c50081f20394b224780ecee4c1c09def359cc2
wolter <sw@saschawolter.de>
2019-03-15 10:35:16 +0100

    linting

--- modules/logging/src/module.ts
+++ modules/logging/src/module.ts
@@ -1,11 +1,11 @@
 /**
- * Logs a message with the given level (see project's log). 
+ * Logs a message with the given level (see project's log).
  * @arg {Select[info,error,debug]} `level` log level
  * @arg {CognigyScript} `message` log message
  * @arg {CognigyScript} `console` log to browser console if channel is webchat (experimental use on your own risk)
  */
-async function log(input: IFlowInput, args: { level: string, message: string, console: string}): Promise<IFlowInput | {}> {  
-    
+async function log(input: IFlowInput, args: { level: string, message: string, console: string}): Promise<IFlowInput | {}> {
+
     input.actions.log(args.level, args.message);
 
     // Hacky way of using the browser console for debugging.
