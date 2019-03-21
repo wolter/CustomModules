44dca3e7f868ae1e202528d1c526f6d4bbf2684e
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-03-21 18:32:06 +0100

    Google Translation Cloud Custom Module

--- modules/googlecloud/README.md
+++ modules/googlecloud/README.md
@@ -1,1 +1,37 @@
-https://www.npmjs.com/package/google-translate
+# Google Cloud
+
+With this module you can use the [Google Cloud API](https://console.cloud.google.com/) to translate or recognize text inside your Cognigy bot — for this, it uses the [Google Translate NPM Package](https://www.npmjs.com/package/google-translate)
+
+**Secrets**
+
+To use the Google Cloud you need to enable specifiy <API KEYS> in your Google Cloud Console.
+
+**Translation, LanguageDetection**
+
+- [Cloud Translation API](https://console.cloud.google.com/apis/library/translate.googleapis.com?q=translation&id=c22f20ba-6a29-40ae-9084-8bc264a97fc2&project=boreal-physics-231713) <API KEY>
+
+**GetGeoLocation**
+
+- [Geolocation API](https://console.cloud.google.com/apis/library/geolocation.googleapis.com?q=maps&id=b87d2884-ba68-47f9-aaf0-b29ae1919bb7&project=boreal-physics-231713) <API KEY>
+
+## Node: Translation
+
+Just choose a **language** in which the **text** should be translated. If you want to use this translation in your later bot conversation, just store it to the CognigyContext.
+
+Example sentence: *Soy Alex y trabajo a Cognigy en Düsseldorf.*
+
+```json
+"translation": "私はAlexです。私はデュッセルドルフのCognigyで働いています。",
+```
+
+
+
+## Node: LanguageDetection
+
+This node detects the language of your given **text** and stores it to the CognigyContext if you want:
+
+Example sentence: *Soy Alex y trabajo a Cognigy en Düsseldorf.*
+
+```json
+"language": "es"
+```
--- modules/googlecloud/src/module.ts
+++ modules/googlecloud/src/module.ts
@@ -1,65 +1,3 @@
-const {google} = require('googleapis');
-const fs = require('fs');
-const readline = require('readline');
-
-/*
-
-Google Sheets methods
-
-*/
-
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = 'token.json';
-
-function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
-}
-
-function getNewToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error while trying to retrieve access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
-}
-
-/*
-
-End of helper methods
-
-*/
-
-
 /**
  * translates a given text
  * @arg {SecretSelect} `secret` The configured secret to use
@@ -79,7 +17,7 @@ async function translation(input: IFlowInput, args: { secret: CognigySecret, lan
   return new Promise((resolve, reject) => {
     let result = {};
 
-    googleTranslate.translate(args.text, args.language, function(err, translation) {
+    googleTranslate.translate(args.text, args.language, function (err, translation) {
       if (err) {
         if (args.stopOnError) { reject(err.message); return; }
         result = { "error": err.message };
@@ -96,7 +34,6 @@ async function translation(input: IFlowInput, args: { secret: CognigySecret, lan
   });
 }
 
-// You have to export the function, otherwise it is not available
 module.exports.translation = translation;
 
 
@@ -118,7 +55,7 @@ async function languageDetection(input: IFlowInput, args: { secret: CognigySecre
   return new Promise((resolve, reject) => {
     let result = {};
 
-    googleTranslate.detectLanguage(args.text, function(err, detection) {
+    googleTranslate.detectLanguage(args.text, function (err, detection) {
       if (err) {
         if (args.stopOnError) { reject(err.message); return; }
         result = { "error": err.message };
@@ -133,69 +70,4 @@ async function languageDetection(input: IFlowInput, args: { secret: CognigySecre
   });
 }
 
-// You have to export the function, otherwise it is not available
 module.exports.languageDetection = languageDetection;
-
-
-/**
- * detects the language of a given sentence
- * @arg {SecretSelect} `secret` The configured secret to use
- * @arg {CognigyScript} `spreadsheetId` of the google sheet
- * @arg {CognigyScript} `range` of the data in the sheet
- * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
- * @arg {CognigyScript} `store` Where to store the result
- * @arg {Boolean} `stopOnError` Whether to stop on error or continue
- */
-async function getDataFromSheet(input: IFlowInput, args: { secret: CognigySecret, spreadsheetId: string, range: string, writeToContext: boolean, store: string, stopOnError: boolean }): Promise<IFlowInput | {}> {
-  // Check if secret exists and contains correct parameters
-  if (!args.secret || !args.secret.key) return Promise.reject("Secret not defined or invalid.");
-  if (!args.spreadsheetId) return Promise.reject("No spreadsheet ID defined.");
-
-  return new Promise((resolve, reject) => {
-    let result = {};
-
-    // Load client secrets from a local file.
-    fs.readFile('credentials.json', (err, content) => {
-      if (err) return console.log('Error loading client secret file:', err);
-      // Authorize a client with credentials, then call the Google Sheets API.
-      authorize(JSON.parse(content), (auth) =>{
-        const sheets = google.sheets({version: 'v4', auth});
-        sheets.spreadsheets.values.get({
-          spreadsheetId: args.spreadsheetId,
-          range: args.range,
-        }, (err, res) => {
-          if (err) {
-            if (args.stopOnError) { reject(err.message); return; }
-            result = { "error": err.message };
-            input.input[args.store] = result;
-            resolve(input);
-          }
-          const rows = res.data.values;
-          if (rows.length) {
-            console.log('Name, Major:');
-            // Print columns A and E, which correspond to indices 0 and 4.
-
-            let body = [];
-            rows.map((row) => {
-              // console.log(`${row[0]}`);
-              body.push(`${row[0]}`)
-            });
-            result = body;
-            if (args.writeToContext) input.context.getFullContext()[args.store] = result;
-            else input.input[args.store] = result;
-            resolve(input);
-          } else {
-            if (args.stopOnError) { reject(err.message); return; }
-            result = { "error": 'No data found.'};
-            input.input[args.store] = result;
-            resolve(input);
-          }
-        });
-      });
-    });
-  });
-}
-
-// You have to export the function, otherwise it is not available
-module.exports.getDataFromSheet = getDataFromSheet;
-
