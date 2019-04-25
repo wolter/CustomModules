50a04490d5d75afef78266dd3a9f2738373fbd99
Alexander Teusz <a.teusz@cognigy.com>
2019-04-25 15:01:12 +0200

    getLocationsByFilter node // works

--- modules/yext/src/module.ts
+++ modules/yext/src/module.ts
@@ -81,3 +81,45 @@ async function GetEntityById(input: IFlowInput, args: { secret: CognigySecret, e
 }
 
 module.exports.GetEntityById = GetEntityById;
+
+
+/**
+ * Gets the information of a chosen table
+ * @arg {SecretSelect} `secret` The configured secret to use
+ * @arg {JSON} `filters` The filters to use for this search
+ * @arg {CognigyScript} `api_version` The version you want to use, e.g. 20190424 (a date)
+ * @arg {Boolean} `stopOnError` Whether to stop on error or continue
+ * @arg {CognigyScript} `store` Where to store the result
+ */
+async function GetLocationsByFilter(input: IFlowInput, args: { secret: CognigySecret, filters: string, api_version: string, stopOnError: boolean, store: string }): Promise<IFlowInput | {}> {
+
+    // Check if secret exists and contains correct parameters
+    if (!args.secret || !args.secret.api_key) return Promise.reject("Secret not defined or invalid.");
+
+    let version = args.api_version || "20190424"
+
+    return new Promise((resolve, reject) => {
+        let result = {};
+        
+        axios.get(`https://api.yext.com/v2/accounts/me/locationsearch?api_key=${args.secret.api_key}&v=${version}`, {
+            headers: {
+                'Allow': 'application/json'
+            },
+            params: {
+                filters: JSON.stringify(args.filters)
+            }  
+        })
+            .then((response) => {
+                result = response.data
+                input.context.getFullContext()[args.store] = result
+                resolve(input)
+            })
+            .catch((error) => {
+                if (args.stopOnError) { reject(error.message); return; }
+                else input.context.getFullContext()[args.store] = { "error": error.message }
+                resolve(input)
+            })
+    });
+}
+
+module.exports.GetLocationsByFilter = GetLocationsByFilter;
