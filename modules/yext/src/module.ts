af5492826ea078cbcd60aed2f391f090af1bac38
Alexander Teusz <a.teusz@cognigy.com>
2019-04-25 14:06:56 +0200

    error handling

--- modules/yext/src/module.ts
+++ modules/yext/src/module.ts
@@ -32,7 +32,7 @@ async function GetEntity(input: IFlowInput, args: { secret: CognigySecret, entit
             })
             .catch((error) => {
                 if (args.stopOnError) { reject(error.message); return; }
-                else result = { "error": error.message };
+                else input.context.getFullContext()[args.store] = { "error": error.message }
                 resolve(input)
             })
     });
@@ -40,3 +40,44 @@ async function GetEntity(input: IFlowInput, args: { secret: CognigySecret, entit
 
 module.exports.GetEntity = GetEntity;
 
+
+/**
+ * Gets the information of a chosen table
+ * @arg {SecretSelect} `secret` The configured secret to use
+ * @arg {Select[Locations,Events,Products,Assets,Entities,Folders,Menus,Bios]} `entity` The entity you want to get from Yext
+ * @arg {CognigyScript} `entity_id` The entitie's id you want to get from Yext
+ * @arg {CognigyScript} `api_version` The version you want to use, e.g. 20190424 (a date)
+ * @arg {Boolean} `stopOnError` Whether to stop on error or continue
+ * @arg {CognigyScript} `store` Where to store the result
+ */
+async function GetEntityById(input: IFlowInput, args: { secret: CognigySecret, entity: string, entity_id: string, api_version: string, stopOnError: boolean, store: string }): Promise<IFlowInput | {}> {
+
+    // Check if secret exists and contains correct parameters
+    if (!args.secret || !args.secret.api_key) return Promise.reject("Secret not defined or invalid.");
+    if (!args.entity) return Promise.reject("No entity defined.");
+
+    let version = args.api_version || "20190424"
+
+    return new Promise((resolve, reject) => {
+        let result = {};
+        
+        axios.get(`https://api.yext.com/v2/accounts/me/${args.entity.toLowerCase()}/${args.entity_id}?api_key=${args.secret.api_key}&v=${version}`, {
+            headers: {
+                'Allow': 'application/json'
+            }
+        })
+            .then((response) => {
+                input.actions.output("", response.data);
+                result = response.data
+                input.context.getFullContext()[args.store] = result
+                resolve(input)
+            })
+            .catch((error) => {
+                if (args.stopOnError) { reject(error.message); return; }
+                else input.context.getFullContext()[args.store] = { "error": error.message }
+                resolve(input)
+            })
+    });
+}
+
+module.exports.GetEntityById = GetEntityById;
